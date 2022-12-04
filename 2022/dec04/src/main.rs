5a1c298a8bc6ba4830a1cea54d792b0efaee3bc7
Johan Brinch <johan.brinch@juni.co>
2022-12-04 10:12:47 +0100

    add dec 04

---
+++ 2022/dec04/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::stdin;
+
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl Range {
+    fn contains(&self, other: &Range) -> bool {
+        self.start <= other.start && other.end <= self.end
+    }
+    fn has_overlap(&self, other: &Range) -> bool {
+        !(self.end < other.start || other.end < self.start)
+    }
+}
+
+fn build_range(fromto: &str) -> Range {
+    let mut ns = fromto.split('-');
+    let start = str::parse(ns.next().unwrap()).unwrap();
+    let end = str::parse(ns.next().unwrap()).unwrap();
+    Range { start, end }
+}
+
+fn main() {
+    let mut count = 0;
+
+    for line_res in stdin().lines() {
+        if let Ok(line) = line_res {
+            let pair: Vec<&str> = line.split(',').collect();
+            let left = build_range(pair[0]);
+            let right = build_range(pair[1]);
+            if left.has_overlap(&right) {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{}", count)
+}
