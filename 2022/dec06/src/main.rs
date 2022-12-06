c923e7a92e52b74ff570b380c9c5c723a89c9296
Johan Brinch <johan.brinch@juni.co>
2022-12-06 09:05:06 +0100

    add dec 06

---
+++ 2022/dec06/src/main.rs
@@ -0,0 +1,22 @@
+use std::{collections::HashSet, hash::Hash, io::stdin};
+
+fn count_unique<E, I>(cs: I) -> usize
+where
+    I: Iterator<Item = E>,
+    E: Eq + Hash,
+{
+    let hs: HashSet<E> = HashSet::from_iter(cs);
+    hs.len()
+}
+
+fn main() {
+    for line in stdin().lines().flatten() {
+        let cs: Vec<char> = line.chars().into_iter().collect();
+        for i in 0..line.len() {
+            if count_unique(cs[i..i + 14].iter()) == 14 {
+                println!("{}", i+14);
+                return;
+            }
+        }
+    }
+}
